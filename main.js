3521e23533a92013bbe06f16d0743104bb8cf29f
Keirarenee <71294118+Keirarenee@users.noreply.github.com>
2021-03-02 17:28:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// silence
+document.addEventListener("DOMContentLoaded", function () {
+  const select_menu = document.querySelector(".filter");
+
+  select_menu.addEventListener("change", function(e){
+    const food_type_class = e.target.value;
+    const active_items = document.querySelectorAll(".item.active");
+
+    active_items.forEach(function(item){
+      item.classList.remove("active");
+    });
+
+    const food_items = document.querySelectorAll(`.item.${food_type_class}`);
+
+    food_items.forEach(function (item){
+      item.classList.add("active");
+    });
+  });
+});
